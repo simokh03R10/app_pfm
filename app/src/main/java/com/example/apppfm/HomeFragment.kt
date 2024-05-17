1d4122f5dfa97a4733fc8ce92ac6470532c60d06
simokh03R10 <youssefkharraz07@gmail.com>
2024-05-17 13:19:38 +0100

    finish

--- app/src/main/java/com/example/apppfm/HomeFragment.kt
+++ app/src/main/java/com/example/apppfm/HomeFragment.kt
@@ -1,10 +1,11 @@
 package com.example.apppfm
 
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
-import android.location.Address
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
@@ -12,21 +13,27 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.apppfm.databinding.FragmentHomeBinding
-import com.example.apppfm.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import java.util.Locale
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(),LocationListener  {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var mContext: Context
-    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var locationManager: LocationManager
+    private val PERMISSION_REQUEST_CODE = 100
+    private var progressDialog: ProgressDialog? = null
 
 
     override fun onAttach(context: Context) {
@@ -41,32 +48,188 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            // Si l'utilisateur est connecté, appelez la fonction de rafraîchissement du contenu
+            refreshContent()
+        }
+
         binding.locationCv.setOnClickListener {
-            val intent = Intent(requireActivity(), LocationPickerActivity::class.java)
-            locationPickerActivityResultLauncher.launch(intent)
+            locationEnabled();
+            showProgressDialog()
+            getLocation();
+        }
+    }
+    private fun showProgressDialog() {
+        progressDialog = ProgressDialog(requireContext())
+        progressDialog?.setMessage("Determination de votre localisation....")
+        progressDialog?.setCancelable(false)
+        progressDialog?.show()
+    }
+    private fun locationEnabled() {
+
+        val lm = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        var gpsEnabled = false
+        var networkEnabled = false
+        try {
+            gpsEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        try {
+            networkEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        if (!gpsEnabled && !networkEnabled) {
+            AlertDialog.Builder(requireContext())
+                .setTitle("Enable GPS Service")
+                .setMessage("We need your GPS location to show Near Places around you.")
+                .setCancelable(false)
+                .setPositiveButton("Enable") { paramDialogInterface, paramInt ->
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+                .setNegativeButton("Cancel", null)
+                .show()
+        }
+
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocation()
+            } else {
+                Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        try {
+            locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 5f, this)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
         }
 
 
     }
 
-    private val locationPickerActivityResultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data = result.data
-                if (data != null) {
-                    val latitude = data.getDoubleExtra("latitude", 0.8)
-                    val longitude = data.getDoubleExtra("longitude", 8.8)
-                    val address = data.getStringExtra("address") ?: ""
-                    binding.locationTv.setText(address)
-                }
-            } else {
-            }
+    override fun onLocationChanged(location: Location) {
+        try {
+            progressDialog?.dismiss()
+            val aLocale: Locale = Locale.Builder().setLanguage("en").setScript("Latn").setRegion("RS").build()
+            val geocoder = Geocoder(requireContext(),aLocale)
+            val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
 
+            // Concaténez le nom de la localité et du pays et affichez-le dans le TextView
+            val locality = addresses?.get(0)?.locality
+            val countryName = addresses?.get(0)?.countryName
+            saveLocationToFirebase(locality, countryName)
+            binding.locationTv.text = "$locality, $countryName"
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+    private fun saveLocationToFirebase(locality: String?, countryName: String?) {
+        // Vérifier si l'utilisateur est connecté à Firebase
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            // Référence à l'utilisateur dans la base de données Firebase
+            val userRef = FirebaseDatabase.getInstance().reference.child("Utilisateurs").child(currentUser.uid)
+
+            // Créer un objet pour stocker les informations de localisation
+            val locationData = HashMap<String, Any>()
+            locationData["location_utilisateur"] = "$locality, $countryName"
+
+            // Mettre à jour le champ location_utilisateur pour l'utilisateur actuel dans Firebase
+            userRef.updateChildren(locationData)
+                .addOnSuccessListener {
+                    // Mise à jour réussie
+                    Log.d("Firebase", "Localisation mise à jour avec succès dans Firebase.")
+                }
+                .addOnFailureListener { e ->
+                    // Échec de la mise à jour
+                    Log.e("Firebase", "Erreur lors de la mise à jour de la localisation dans Firebase : ${e.message}")
+                }
+        }
+    }
+
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+
+    override fun onProviderEnabled(provider: String) {}
+
+    override fun onProviderDisabled(provider: String) {}
+    override fun onResume() {
+        super.onResume()
+        // Vérifier les autorisations de localisation à nouveau si nécessaire
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getLocation()
+        }
+
+        // Récupérer la localisation depuis Firebase de manière asynchrone
+        getLocationFromFirebase { location ->
+            // Mettre à jour l'interface utilisateur avec la nouvelle localisation
+            binding.locationTv.text = location
+        }
+    }
+    fun refreshContent() {
+        // Affichez une boîte de dialogue de chargement ou un indicateur de chargement si nécessaire
+        showProgressDialog()
+        progressDialog?.dismiss()
+        // Récupérez la localisation depuis Firebase
+        getLocationFromFirebase { location ->
+            // Mettez à jour l'affichage avec la nouvelle localisation
+
+            binding.locationTv.text = location
+
+            // Masquez la boîte de dialogue de chargement ou l'indicateur de chargement une fois le rafraîchissement terminé
 
         }
+    }
+
+    private fun getLocationFromFirebase(callback: (String) -> Unit) {
+        // Vérifier si l'utilisateur est connecté à Firebase
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        if (currentUser != null) {
+            // Référence à l'utilisateur dans la base de données Firebase
+            val userRef = FirebaseDatabase.getInstance().reference.child("Utilisateurs").child(currentUser.uid)
+
+            // Écouter les modifications de la localisation de l'utilisateur dans Firebase
+            userRef.child("location_utilisateur").addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // Récupérer la localisation de l'utilisateur
+                    val location = dataSnapshot.value.toString()
+                    // Appeler le callback avec la localisation récupérée depuis Firebase
+                    callback(location)
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // En cas d'annulation
+                    Log.e("Firebase", "Erreur lors de la récupération de la localisation depuis Firebase : ${databaseError.message}")
+                }
+            })
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            // Si l'utilisateur est connecté, appelez la fonction de rafraîchissement du contenu
+            refreshContent()
+        }
+    }
+
+
 }
+
+
--- app/src/main/java/com/example/apppfm/MainActivity.kt
+++ app/src/main/java/com/example/apppfm/MainActivity.kt
@@ -9,54 +9,66 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
 
+        // Vérifier si l'utilisateur est connecté, sinon rediriger vers l'activité de bienvenue
         if (auth.currentUser == null) {
-            // If user is not logged in, redirect to LoginActivity
             startActivity(Intent(this, WelcomeActivity::class.java))
             finish()
             return
         }
 
-        binding.buttonsellfragement.setOnClickListener(){
-            startActivity(Intent(this, CreatepostActivity::class.java))
-            finish()
-        }
-
+        // Gérer les sélections d'éléments du BottomNavigationView
         binding.bottomnavicons.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menu_home -> {
                     binding.textviewtitle.text = "Home"
-                    supportFragmentManager.beginTransaction().replace(R.id.container, HomeFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, HomeFragment())
+                        .addToBackStack(null)
+                        .commit()
                     true
                 }
                 R.id.menu_favoris -> {
                     binding.textviewtitle.text = "Mes Favoris"
-                    supportFragmentManager.beginTransaction().replace(R.id.container, FavorisFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, FavorisFragment())
+                        .addToBackStack(null)
+                        .commit()
                     true
                 }
-
                 R.id.menu_chat -> {
                     binding.textviewtitle.text = "Mes Messages"
-
-                    supportFragmentManager.beginTransaction().replace(R.id.container, ChatFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, ChatFragment())
+                        .addToBackStack(null)
+                        .commit()
                     true
                 }
                 R.id.menu_profile -> {
                     binding.textviewtitle.text = "Mon profil"
-                    supportFragmentManager.beginTransaction().replace(R.id.container, ProfileFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, ProfileFragment())
+                        .addToBackStack(null)
+                        .commit()
                     true
                 }
                 else -> false
             }
         }
 
+        // Sélectionner l'élément de menu "Home" par défaut si aucun état n'est sauvegardé
+        if (savedInstanceState == null) {
+            binding.bottomnavicons.selectedItemId = R.id.menu_home
+        }
     }
 }
+
 /*
 1) Create & Setup the Project
 2) Bottom Navigation Menu
